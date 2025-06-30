dc820793f1d3a4793b4737ee2490dd3716f3979d
Karthik Rachamolla <134269866+kee5625@users.noreply.github.com>
2025-06-30 17:58:08 +0000

    created server folder and added all required npm packages. Yet to configure a requirements page for easier development.

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -32,7 +32,7 @@ const ProfilePage = () => {
           <textarea rows={4} onChange={(e) => setBio(e.target.value)} value={bio}
           required placeholder='Write profile bio' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500'></textarea>
 
-          <button type='submit' className='bg-graident-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>Save</button>
+          <button type='submit' className='bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>Save</button>
         </form>
         <img className='max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10' src={assets.logo_icon} />
       </div>
