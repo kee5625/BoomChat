d71f686ac15c187a7fad6d1dab137ac1479fa92b
Karthik Rachamolla <134269866+kee5625@users.noreply.github.com>
2025-07-02 19:04:27 +0000

    updated authcontext and modified app.jsx routes to include authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import { Route, Routes} from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Navigate, Route, Routes} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import bgImage from './assets/bgImage.svg';
+import {Toaster} from "react-hot-toast";
+import { AuthContext } from '../context/AuthContext'
 
 const App = () => {
+  const {authUser} = useContext(AuthContext);
   return (
     <div
       style={{backgroundImage: `url(${bgImage})`}} 
       className="bg-center w-full border border-b bg-contain min-h-screen">
+      <Toaster />
       <Routes>
         <Route>
           {/* Base Routing */}
-          <Route path='/' element={<HomePage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/profile' element={<ProfilePage />} />
-
-
-
+          <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+          <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+          <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
         </Route>
       </Routes>
     </div>
